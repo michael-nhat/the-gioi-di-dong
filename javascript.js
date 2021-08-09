cd80bd166827980895c06933205ee77f0c2a1a0c
Hoang Van Nhat <nhathoang.haiphong@gmail.com>
2021-08-09 21:09:22 +0700

    first week

---
+++ javascript.js
@@ -0,0 +1,12 @@
+var slideIndex = 1;
+showDiv(slideIndex);
+
+function plusDivs(n) {
+    showDiv(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x =
+    document.getElementsByClassName("")
+}
